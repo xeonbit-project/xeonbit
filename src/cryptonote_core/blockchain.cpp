b917b61d2f5ca201ea134b713747ca33299ce01f
Joseph William <31919101+Coinsman@users.noreply.github.com>
2019-01-05 01:06:45 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -94,10 +94,10 @@ static const struct {
   { 1, 1, 0, 1546473600 },
   
   // versions 2, 3, 4, 5,6 and 7 are skipped, in favor of reducing the cost of adopting the POW change and other consensus updates from Xeonbit
-  // version 8 starts from block 652160, which is on or around the 4th of Jan, 2018. Fork time finalised on 2019-01-09.
+  // version 8 starts from block 652164, which is on or around the 4th of Jan, 2018. Fork time finalised on 2019-01-09.
   { 8, 652165, 0, 1546992000},
 };
-static const uint64_t mainnet_hard_fork_version_1_till = 652165;
+static const uint64_t mainnet_hard_fork_version_1_till = 652164;
 
 static const struct {
   uint8_t version;
